74f6eaf91129e31708fa81cd13e7decd0713de81
Radeniss~ <radenis354@gmail.com>
2025-11-21 23:34:53 +0700

    update

--- components/PortofolioGrid.tsx
+++ components/PortofolioGrid.tsx
@@ -6,7 +6,7 @@ export default function PortofolioGrid ({portofolioItems}) {
     const [isExpanded, setIsExpanded] = useState(false);
     const fullText = portofolioItems.desc.join('\n\n');
     const characterLimit = 170;
-    const isToolong = fullText.leghth > characterLimit;
+    const isToolong = fullText.length > characterLimit;
 
     const displayText = isToolong && !isExpanded
     ? fullText.slice(0, characterLimit) + '...'
@@ -21,6 +21,15 @@ export default function PortofolioGrid ({portofolioItems}) {
                     key={item.id}
                     className='bg-white rounded-xl shadow-lg overflow-hidden transition-shadow duration-300 hier:shadow-x1 border border-gray-100'
                 >
+                    <div className='realtive w-full h-48'>
+                        <img
+                            src={item.img}
+                            alt={item.title}
+                            layout='fill'
+                            objectFit='cover'
+                            className='trasnsition-transform duration-500 hover:scale-105'
+                        />
+                    </div>
                 </div>
             ))}
         </div>
